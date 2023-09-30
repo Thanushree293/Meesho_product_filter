61096c64bdf0ad267a324fe7a0e37b2d5a429ed5
thanushree <thanushree.vaishnavi@gmail.com>
2023-09-30 15:08:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,315 @@
+let products /*object*/ = {
+    /*property name called*/data:[
+        {
+            productName: "CUMFFY TOP AND BOTTOM Lehanga",
+            category: "Lehanga",
+            price:"678",
+            image:"images/lehanga1.png",
+            link:"https://www.meesho.com/cumffy-top-and-bottom/p/3KUY21",
+        },
+        {
+            productName: "WOMEN HEAVY LEHNGA CHOLI",
+            category: "Lehanga",
+            price:"1,474",
+            image:"images/lehanga2.jpg",
+            link:"https://www.meesho.com/women-fency-lehnga-choli/p/3JAGFO",
+        },
+        {
+            productName: "Simple Adrika Attractive Women Lehenga",
+            category: "Lehanga",
+            price:"920",
+            image:"images/lehanga3.png",
+            link:"https://www.google.com/shopping/product/7920405640355215234?q=meesho+lehenga&prds=epd:8716269636005636475,eto:8716269636005636475_0,pid:5223605054182565382&sa=X&ved=0ahUKEwiH-7uK-7eAAxUFcmwGHbb6A5YQ9pwGCAU",
+        },
+        {
+            productName: "Party wear Lehenga (Color-Wine) ",
+            category: "Lehanga",
+            price:"739",
+            image:"images/lehanga4.png",
+            link:"https://www.meesho.com/party-wear-lehenga-color-wine/p/18L5V4",
+        },
+        {
+            productName: "fancy trending lengha choli",
+            category: "Lehanga",
+            price:"1,129",
+            image:"images/lehanga5.png",
+            link:"https://www.meesho.com/fancy-trending-lengha-choli/p/55RCXF",
+        },
+        {
+            productName: "New Fancy Lehenga(Black)",
+            category: "Lehanga",
+            price:"949",
+            image:"images/lehanga6.png",
+            link:"https://www.meesho.com/new-fancy-lehenga/p/57M3PP",
+        },{
+        productName: "Simple NET WITH CHAIN EMBROIDERY LEHENGHA",
+            category: "Lehanga",
+            price:"614",
+            image:"images/lehanga7.png",
+            link:"https://www.meesho.com/simple-net-with-chain-embroidery-lehengha/p/9YODJ",
+        },
+        {
+        productName: "Net Embroidered Semi-Stitched Lehenga Choli with Duppatta",
+            category: "Lehanga",
+            price:"646",
+            image:"images/lehanga8.png",
+            link:"https://www.meesho.com/net-embroidered-semi-stitched-lehenga-choli-with-duppatta/p/5870WC",
+        },
+        {
+        productName: "Embroidered Silk Bridal Lehenga Choli",
+            category: "Lehanga",
+            price:"717",
+            image:"images/lehanga9.png",
+            link:"https://www.meesho.com/embroidered-silk-bridal-lehenga-choli/p/57LYRP",
+        },
+        {
+        productName: "Traditinal Pattu Lehenga choli",
+            category: "Lehanga",
+            price:"905",
+            image:"images/lehanga10.png",
+            link:"https://www.meesho.com/traditinal-pattu-lehenga-choli/p/1SIEXK",
+        },
+        {
+            productName: "Waist Coat For Men",
+                category: "Shirts",
+                price:"368",
+                image:"Waist coat for men.png",
+                link:"https://www.meesho.com/waist-coat-for-men/p/4ke6jv",
+        },
+        {
+            productName: "Men's Trendy Half Sleeves Hooded Neck Printed With Typography Red T-Shirt",
+                category: "Shirts",
+                price:"290",
+                image:"Men's Trendy Half Sleeves Hooded Neck Printed With Typography Red T-Shirt.png",
+                link:"https://www.meesho.com/mens-trendy-half-sleeves-hooded-neck-printed-with-typography-red-t-shirt/p/1e9y66",
+        },
+        {
+            productName: "Helmont Men's Solid Round Black Tshirts",
+                category: "Shirts",
+                price:"179",
+                image:"Helmont Men's Solid Round Black Tshirts.png",
+                link:"https://www.meesho.com/helmont-mens-solid-round-black-tshirts/p/4dbhaf",
+        },
+        {
+            productName: "Aarti Collection best trending Mens Tshirts",
+                category: "Shirts",
+                price:"217",
+                image:"Aarti Collection best trending Mens Tshirts .png",
+                link:"https://www.meesho.com/aarti-collection-best-trending-mens-tshirts-printed-mens-full-sleeve-tshirts/p/3j0a6q",
+        },
+        {
+            productName: "Classy Retro Men Tshirts",
+                category: "Shirts",
+                price:"135",
+                image:"Classy Retro Men Tshirts.png",
+                link:"https://www.meesho.com/classy-retro-men-tshirts/p/39z5ey",
+        },
+        {
+            productName: "REE RAA Off Shoulder Colorblock Tshirt for Mens",
+                category: "Shirts",
+                price:"292",
+                image:"REE RAA Off Shoulder Colorblock Tshirt for Mens.png",
+                link:"https://www.meesho.com/ree-raa-off-shoulder-colorblock-tshirt-for-mens/p/2lcrs9",
+        },
+        {
+            productName: "Trendy Retro Women Tops & Tunics",
+                category: "Tops",
+                price:"251",
+                image:"Trendy Retro Women Tops & Tunics.png",
+                link:"https://www.meesho.com/trendy-retro-women-tops-tunics/p/fhqnw",
+        },
+        {
+            productName: "yellow bandej top 03",
+                category: "Tops",
+                price:"246",
+                image:"yellow bandej top 03.png",
+                link:"https://www.meesho.com/yellow-bandej-top-03/p/365hut",
+        },
+        {
+            productName: "TANK TOP BTTR BLUE RIB",
+                category: "Tops",
+                price:"193",
+                image:"TANK TOP BTTR BLUE RIB.png",
+                link:"https://www.meesho.com/tank-top-bttr-blue-rib/p/56rno5",
+        },
+        {
+                productName: "texfield present stylish croptop",
+                category: "Tops",
+                price:"305",
+                image:"texfield present stylish croptop.png",
+                link:"https://www.meesho.com/texfield-present-stylish-croptop/p/57aavl",
+        },
+        {
+                productName: "Stylish Retro Women Tops & Tunics",
+                category: "Tops",
+                price:"198",
+                image:"Stylish Retro Women Tops & Tunics.png",
+                link:"https://www.meesho.com/stylish-retro-women-tops-tunics/p/1oavii",
+        },
+        {
+                productName: "BLACK JEANS FOR MEN",
+                category: "Jeans",
+                price:"371",
+                image:"BLACK JEANS FOR MEN.png",
+                link:"https://www.meesho.com/black-jeans-for-men/p/2xtwh9",
+        },
+        {
+                productName: "jeans",
+                category: "Jeans",
+                price:"431",
+                image:"Jeans.png",
+                link:"https://www.meesho.com/jeans/p/2hm51k",
+        },
+        {
+            productName: "Men jeans Stretchable Slim-fit Original Essentials.",
+            category: "Jeans",
+            price:"512",
+            image:"images/Men jeans Stretchable Slim-fit Original Essentials..pg.jpg",
+            link:"https://www.meesho.com/men-jeans-stretchable-slim-fit-original-essentials/p/22iix5",
+        },
+        {
+            productName: "Women combo sling bag",
+            category: "bags",
+            price:"255",
+            image:"Women combo sling bag.png",
+            link:"https://www.meesho.com/women-combo-sling-bag/p/3s5mnh",
+        },
+        {
+            productName: "pineapple girls bag",
+            category: "bags",
+            price:"255",
+            image:"pineapple girls bag.png",
+            link:"https://www.meesho.com/pineapple-girls-bag-2/p/4jnpws",
+        },
+        {
+            productName: "FANCY CLUTCH",
+            category: "bags",
+            price:"464",
+            image:"FANCY CLUTCH.png",
+            link:"https://www.meesho.com/fancy-clutch/p/53szga",
+        },
+        {
+            productName: "Small 10L Premium Backpack For Girls PU",
+            category: "bags",
+            price:"309",
+            image:"Small 10L Premium Backpack For Girls PU Leather.png",
+            link:"https://www.meesho.com/small-10l-premium-backpack-for-girls-pu-leather/p/144zbs",
+        },
+        {
+            productName: "multicolour patti print Medium 25 L Laptop Backpack",
+            category: "bags",
+            price:"303",
+            image:"multicolour patti print Medium 25 L Laptop Backpack.png",
+            link:"https://www.meesho.com/multicolour-patti-print-medium-25-l-laptop-backpack/p/1gslfj",
+        },
+        {
+            productName: "Black Printed Sneakers For Men",
+            category: "shoes",
+            price:"266",
+            image:"Black Printed Sneakers For Men.png",
+            link:"https://www.meesho.com/black-printed-sneakers-for-men/p/1xyz0h",
+        },
+        {
+            productName: "Latest trendy 2023 fighter white shoes for girls and women",
+            category: "shoes",
+            price:"349",
+            image:"Latest trendy 2023 fighter white shoes for girls and women.png",
+            link:"https://www.meesho.com/latest-trendy-2023-fighter-white-shoes-for-girls-and-women/p/4pmtsn",
+        },
+        {
+            productName: "Sport Shoes For Women's & Girls",
+            category: "shoes",
+            price:"383",
+            image:"Sport Shoes For Women's & Girls.png",
+            link:"https://www.meesho.com/latest-stylish-women-casual-sneakers-sport-shoes-for-womens-girls/p/37x1oc",
+        },
+        {
+            productName: "Men Denim Casual Sneakers Shoes",
+            category: "shoes",
+            price:"479",
+            image:"Men Denim Casual Sneakers Shoes.png",
+            link:"https://www.meesho.com/men-denim-casual-sneakers-shoes/p/gvnhx",
+        },
+    ]}
+  
+
+    for(let i of products.data){
+        let link= document.createElement("a");
+        link.setAttribute("href",i.link);
+        link.setAttribute("target","_block");
+        let card = document.createElement("div");
+        card.classList.add("card",i.category,"hide");
+        let imgContainer=document.createElement("div");
+        imgContainer.classList.add("img-container");
+        let image = document.createElement("img");
+        image.setAttribute("src",i.image);
+        imgContainer.appendChild(image);
+        link.appendChild(imgContainer)
+        card.appendChild(link);
+        let txt = document.createElement("div");
+        txt.classList.add("txt");
+        let name = document.createElement("h5");
+        name.classList.add("product-name");
+        name.innerText = i.productName.toUpperCase();
+        txt.appendChild(name);
+        let price = document.createElement("h6");
+        price.innerText = "₹ "+i.price;
+        txt.appendChild(price);
+        card.appendChild(txt);
+        // link.appendChild(card);
+        document.getElementById("Products").appendChild(card);
+    }
+    
+
+
+    function filterItems(value){
+        let button = document.querySelectorAll(".nav-btn");
+        button.forEach((btn)=>{
+            if(value.toUpperCase() == btn.innerHTML.toUpperCase()){
+                btn.classList.add("active");
+            }
+            else{
+                btn.classList.remove("active")
+            }
+        });
+
+        let elements = document.querySelectorAll(".card");
+        elements.forEach((ele)=>{
+            if(value == "all"){
+                ele.classList.remove("hide");
+            }
+            else{
+                if(ele.classList.contains(value)){
+                    ele.classList.remove("hide");
+                    console.log(value);
+                }
+                else{
+                    ele.classList.add("hide");
+                }
+            }
+        });
+    }
+
+document.getElementById("search-btn").addEventListener("click",()=>{
+    let searchInput = document.getElementById("search").value;
+    let input = document.querySelectorAll(".product-name");
+    let cards = document.querySelectorAll(".card");
+
+    input.forEach((element,index)=>{
+        if(element.innerText.includes(searchInput.toUpperCase())){
+            cards[index].classList.remove("hide");
+        }else{
+            cards[index].classList.add("hide");
+        }
+    });
+});
+
+window.onload=()=>{
+    filterItems("all");
+}
+        
+// document.getElementById("playstore").addEventListener("click",()=>{
+//     let a =document.createElement("a")
+//     a.setAttribute("href","https://play.google.com/store/apps/details?id=com.meesho.supply&pow_distinct_id=189984233fdc53-0d4dd337360112-26031c51-144000-189984233fe178e&pow_click_page_type=PDP&c=pow&pid=pow_website&pow_click_page_title=FIERA%20FASHION%20Girls%20Red%20Creap%20Frocks");
+//     a.appendChild(document.getElementById("playstore"));
+// })
